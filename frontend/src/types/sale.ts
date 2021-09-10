94e8bca2f11e3606bbd950b248f1293ffbf67475
jessica klier <jhessicadesouza12@hotmail.com>
2021-09-10 16:41:09 -0300

    Barchart integration

--- frontend/src/types/sale.ts
+++ frontend/src/types/sale.ts
@@ -1,4 +1,11 @@
 export type SaleSum =  {
-    sellerName: string,
-    sum: number
+    sellerName: string;
+    sum: number;
 }
+
+export type SaleSucess = {
+    sellerName : string;
+		visited : number;
+		deals : number;
+}
+// talves de errado , ai terei que ir no postman refazer a parte do sucess.
